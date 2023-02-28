fe72b6c24e0bfc3bb8c09b71679e9e980a9ce46a
tomgif <tomgif@rambler.ru>
2023-02-28 16:15:58 +0300

    clear

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,6 @@
 const {defineConfig} = require('@vue/cli-service')
+
 module.exports = defineConfig({
-    runtimeCompiler: true,
     transpileDependencies: true,
-    publicPath: '/dice/',
-    chainWebpack: config => {
-        config.module
-            .rule('vue')
-            .use('vue-loader')
-            .tap(options => ({
-                ...options,
-                isCustomElement: tag => ['x-g'].includes(tag)
-            }))
-    }
+    publicPath: '/dice/'
 })
